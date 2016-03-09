d26b98e787d31c730c02a441044572af58439ad0
Jacob H. Haven <jacob@cloudflare.com>
2016-03-09 14:33:31 -0800

    Add full ECDSA PrivateKey support to openpgp

--- openpgp/brainpool_test.go
+++ openpgp/brainpool_test.go
@@ -1,48 +1,65 @@
 package openpgp
 
 import (
+	"bytes"
+	"crypto"
 	"os"
+	"strings"
 	"testing"
 
-	"github.com/keybase/go-crypto/openpgp/armor"
 	"github.com/keybase/go-crypto/openpgp/packet"
 )
 
-func parseKeyFile(t *testing.T, name string) {
+const msg = "Hello World!"
+
+func signWithKeyFile(t *testing.T, name, password string) {
 	f, err := os.Open(name)
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer f.Close()
-
-	b, err := armor.Decode(f)
+	es, err := ReadArmoredKeyRing(f)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	p, err := packet.Read(b.Body)
+	// Cycle through all entities we find a signing key.
+	for _, e := range es {
+		if err = e.PrivateKey.Decrypt([]byte(password)); err != nil {
+			continue
+		}
+
+		buf := new(bytes.Buffer)
+		if err = DetachSign(buf, e, strings.NewReader(msg), nil); err == nil {
+			p, err := packet.Read(buf)
+			if err != nil {
+				t.Fatal(err)
+			}
+			sig, ok := p.(*packet.Signature)
+			if !ok {
+				t.Fatal("couldn't parse signature from buffer")
+			}
+			signed := crypto.SHA256.New()
+			signed.Write([]byte(msg))
+			if err := e.PrimaryKey.VerifySignature(signed, sig); err != nil {
+				t.Fatal(err)
+			}
+
+			break
+		}
+	}
 	if err != nil {
 		t.Fatal(err)
-	}
-
-	priv, ok := p.(*packet.PrivateKey)
-	if !ok {
-		t.Fatal("didn't parse a private key")
-	}
-
-	if priv.PubKeyAlgo != packet.PubKeyAlgoECDSA {
-		t.Fatal("not an ecdsa key")
 	}
 }
 
 func TestParseP256r1(t *testing.T) {
-	parseKeyFile(t, "testdata/brainpoolP256r1.pgp")
+	signWithKeyFile(t, "testdata/brainpoolP256r1.pgp", "256")
 }
 
 func TestParseP384r1(t *testing.T) {
-	parseKeyFile(t, "testdata/brainpoolP384r1.pgp")
+	signWithKeyFile(t, "testdata/brainpoolP384r1.pgp", "384")
 }
 
 func TestParseP512r1(t *testing.T) {
-	parseKeyFile(t, "testdata/brainpoolP512r1.pgp")
+	signWithKeyFile(t, "testdata/brainpoolP512r1.pgp", "512")
 }
