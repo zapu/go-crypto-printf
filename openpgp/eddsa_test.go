a774d9558a2a65b7ddc19b93857eb70b9d938f42
Michał Zochniak <michal@zapu.net>
2017-03-03 16:33:46 +0100

    Add tests for EdDSA

---
+++ openpgp/eddsa_test.go
@@ -0,0 +1,139 @@
+package openpgp
+
+import (
+	"bytes"
+	"io"
+	"strings"
+	"testing"
+
+	"github.com/keybase/go-crypto/openpgp/armor"
+	"github.com/keybase/go-crypto/openpgp/clearsign"
+	"github.com/keybase/go-crypto/openpgp/packet"
+)
+
+const ed25519SecretKey = `-----BEGIN PGP PRIVATE KEY BLOCK-----
+
+lFgEWLlH5hYJKwYBBAHaRw8BAQdAZok35G7RiRPTVW+V4B6+cS6B0YTHoCdiZ65o
+GLXR8pAAAP4qdiV8Ib+oJfhikpP4k81Lse0uhiKztT+i8VbCf7f8CRIPtB5FZERT
+QSBLZXliYXNlIGdvLWNyeXB0byB0ZXN0ZXKIeQQTFggAIQUCWLlH5gIbAwULCQgH
+AgYVCAkKCwIEFgIDAQIeAQIXgAAKCRBGOGQrdSAHN7jKAQCrAZLd1DfOsLRHfLqs
+2/n1HlX55I7G6aG70Nmp+IFcQAEAoRJcdiSWVLAzXbYpKb3QOl2THqbQScfkKZ6D
+NyJqNA4=
+=HtLj
+-----END PGP PRIVATE KEY BLOCK-----
+`
+
+func TestEd25519RoundTrip(t *testing.T) {
+	testString := "test okokokokokok"
+
+	entities, err := ReadArmoredKeyRing(strings.NewReader(ed25519SecretKey))
+	if err != nil {
+		t.Fatalf("error opening keys: %v", err)
+	}
+	if len(entities) != 1 {
+		t.Fatal("expected only 1 key")
+	}
+	if !entities[0].PrimaryKey.PubKeyAlgo.CanSign() {
+		t.Fatal("key cannot sign")
+	}
+	buf := new(bytes.Buffer)
+	err = ArmoredDetachSign(buf, entities[0], bytes.NewBufferString(testString), nil)
+
+	block, err := armor.Decode(buf)
+	if err != nil {
+		t.Fatalf("error decoding resulting armor: %v", err)
+	}
+	reader := packet.NewReader(block.Body)
+	pkt, err := reader.Next()
+	if err != nil {
+		t.Fatalf("error reading armored signature: %v", err)
+	}
+
+	sig, ok := pkt.(*packet.Signature)
+	if !ok {
+		t.Fatalf("signature did not parse")
+	}
+
+	hash := sig.Hash.New()
+	io.Copy(hash, bytes.NewBufferString(testString))
+
+	err = entities[0].PrimaryKey.VerifySignature(hash, sig)
+	if err != nil {
+		t.Fatalf("verify signature failed with: %v", err)
+	}
+}
+
+// Clearsigned message for key that we own.
+const clearSignPayload = `-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA256
+
+OpenPGP is the most widely used email encryption standard. It is
+defined by the OpenPGP Working Group of the Internet Engineering 
+Task Force (IETF) as a Proposed Standard in RFC 4880. OpenPGP was
+originally derived from the PGP software, created by Phil Zimmermann.
+-----BEGIN PGP SIGNATURE-----
+
+iF4EARYIAAYFAli5X9EACgkQRjhkK3UgBze3fgEAg84Kcps0b70/6tMqc5TgslCJ
+UeS5HBl7HiKTtkPykCcBALQOjckckdBz6KxAhKxmjOJ8qxcbx30Ye2A7nYtyohoI
+=lv2c
+-----END PGP SIGNATURE-----
+`
+
+// Clearsigned message (altered), signature is invalid
+const clearSignPayloadInvalid = `-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA256
+
+Chancellor on brink of second bailout for banks
+-----BEGIN PGP SIGNATURE-----
+
+iF4EARYIAAYFAli5WNAACgkQRjhkK3UgBzdK4AEA/iTUhqia9hlAjzbfhG1Y5IQF
+XkvWqK865ABexLnGDSAA/2Z5QWG4PUqLQZl5fY5Zuv452e4NWqY35uUCxYw/eHEB
+=kaTQ
+-----END PGP SIGNATURE-----
+`
+
+// Clearsigned message for different key
+const clearSignPayloadNoKey = `-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA256
+
+Hello World!
+-----BEGIN PGP SIGNATURE-----
+
+iF4EARYIAAYFAli5YNYACgkQH8B3H/TuEEdFsQEAzTu7RlxmXuZXn4ZDrGHDmm+O
+3wNPA6jXWCOw8raTQMgA/22jmf9Zje25p10p1ULpMfHiCBwabu5u6GFVrWwk9xsG
+=qh4w
+-----END PGP SIGNATURE-----
+`
+
+func readKeysAndCheckSig(clearsigned string, t *testing.T) (err error) {
+	block, _ := clearsign.Decode(bytes.NewBufferString(clearsigned).Bytes())
+
+	entities, err := ReadArmoredKeyRing(strings.NewReader(ed25519SecretKey))
+	if err != nil {
+		t.Fatalf("error opening keys: %v", err)
+	}
+
+	_, err = CheckDetachedSignature(entities, bytes.NewBuffer(block.Bytes), block.ArmoredSignature.Body)
+	return err
+}
+
+func TestEd25519VerifyClearSign(t *testing.T) {
+	err := readKeysAndCheckSig(clearSignPayload, t)
+	if err != nil {
+		t.Fatalf("error checking signature: %v", err)
+	}
+}
+
+func TestEd25519VerifyClearSignInvalid(t *testing.T) {
+	err := readKeysAndCheckSig(clearSignPayloadInvalid, t)
+	if err == nil {
+		t.Fatalf("verification should fail!")
+	}
+}
+
+func TestEd25519VerifyClearSignNoKey(t *testing.T) {
+	err := readKeysAndCheckSig(clearSignPayloadNoKey, t)
+	if err == nil {
+		t.Fatalf("we should not be able to verify this!")
+	}
+}
